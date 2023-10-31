c9d27b78a630465fc2ce01d4f877295277a3a184
gagan-chauhan <gagan2020chauhan@gmail.com>
2023-10-31 09:57:58 +0530

    the end

--- weather-app/src/components/Template.jsx
+++ weather-app/src/components/Template.jsx
@@ -30,7 +30,7 @@ const Template = () => {
     }, [city]);
 
     return (
-        <div className="w-9/12 mx-auto p-4 bg-white rounded-lg shadow-md mt-40">
+        <div className="w-9/12 mx-auto p-4 bg-white rounded-lg shadow-md mt-10 md:mt-20 lg:mt-40">
             <div className="city-search">
                 <input
                     type="search"
@@ -45,20 +45,20 @@ const Template = () => {
             {loading && <p className="mt-4 text-center">Loading...</p>}
             {weather.main && (
                 <div className="content-show mt-4 flex flex-col">
-                    <div className='main flex flex-row justify-evenly'>
-                        <div className='city flex flex-col justify-center text-7xl font-bold'>
+                    <div className='main flex flex-col md:flex-col justify-evenly'>
+                        <div className='city flex md:flex-col flex-row justify-center text-3xl md:text-2xl font-bold'>
                             {weather.name}
                         </div>
                         <div className='about'>
-                            <div className="temp-degree text-8xl text-center font-bold">
+                            <div className="temp-degree text-7xl md:text-8xl text-center font-bold">
                                 {(weather.main.temp - 273.15).toFixed(1)}°C
                             </div>
-                            <div className="weather-visible mt-2 text-3xl text-center font-bold">
+                            <div className="weather-visible mt-2 text-2xl md:text-3xl text-center font-bold">
                                 {weather.weather[0].description}
                             </div>
                         </div>
                     </div>
-                    <div className="more mt-5 flex flex-row gap-5 justify-center text-lg">
+                    <div className="more mt-5 flex flex-col md:flex-row gap-5 justify-center text-base md:text-lg">
                         <div className="humidity">
                             Humidity: {weather.main.humidity}%
                         </div>
